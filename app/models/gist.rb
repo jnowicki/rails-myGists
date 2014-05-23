c99cef6b04a451e2524bbd1a1ffa90b8626bbc2b
Kuba <kuba@kuba-VirtualBox.(none)>
2014-05-23 10:36:30 +0200

    "poprawilem travis depreciated warning"

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -4,9 +4,11 @@ class Gist < ActiveRecord::Base
 
 	def self.search(search, user)
   		if search
-    		find(:all, :conditions => ['(user_id = ? OR status = ?) AND description LIKE ?', user, 'public', "%#{search}%"])
+    		#find(:all, :conditions => ['(user_id = ? OR status = ?) AND description LIKE ?', user, 'public', "%#{search}%"])
+    		where(['(user_id = ? OR status = ?) AND description LIKE ?', user, 'public', "%#{search}%"]).to_a
   		else
-    		find(:all, :conditions => ['user_id = ? OR status = ?', user, 'public'])
+    		#find(:all, :conditions => ['user_id = ? OR status = ?', user, 'public'])
+    		where(['user_id = ? OR status = ?', user, 'public']).to_a
   		end
 	end
 end
